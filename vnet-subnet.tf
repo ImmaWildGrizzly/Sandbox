a2b80619be7d7856e84b713d2edb55bb18d120df
DeAngelo <mrgrizzly76@gmail.com>
2024-06-15 08:32:41 -0400

    Update

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "TerraformSubnet" {
     name =var.subnetname
     resource_group_name = azurerm_resource_group.Terraform.name
 
-    virtual_network_name = azurerm_virtual_network.TerraformNetwork
+    virtual_network_name = azurerm_virtual_network.TerraformNetwork.name
 
     address_prefixes = ["10.0.2.0/24"]
 }
